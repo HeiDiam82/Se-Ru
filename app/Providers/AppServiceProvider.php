9748fd895349161d2e48d59b8cc3d50867419809
Farrel Adhi Pratama <akuturu580@gmail.com>
2026-05-17 07:18:45 -0700

    mengubah dari http:// di url CSSnya ke https://

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Railway menggunakan reverse proxy yang menghandle SSL termination.
+        // Tanpa ini, Laravel akan generate URL http:// yang diblokir browser (Mixed Content Error).
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
